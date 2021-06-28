eb4e79c7a0f7b264481e58293943557f275e35ed
jayseearr <mramazingx@gmail.com>
2021-06-28 11:17:00 -0400

    added comments to describe cacheMatrix creation and usage. Some error fixes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R contains functions used in the creation of cacheMatrix 
+## objects. A cacheMatrix has the functionality of a matrix object, but stores
+## the inverse of the matrix (obtained via the 'solve' function) in a
+## persistent variable once it has been solved for once. 
+##
+## Example usage:
+## N <- 100
+## X <- matrix(rnorm(N*N),N,N)   # create a standard NxN matrix with N*N random elements.
+## Y <- makeCacheMatrix(X)       # create a cacheMatrix identical to the matrix X.  
+## Yi <- cacheSolve(Y)           # returns the inverse of Y (analogous to solve(X)).
+##
+## Y$get()      # returns the NxN matrix
+## Y$getinv()   # returns NULL if the inverse has not been solved for yet, and the inverse of X otherwise (equal to solve(X))
+
 
 ## makeCacheMatrix returns a matrix that has a cached inverse. This is useful
 ## for large matrices, where computing the inverse (using the 'solve' function)
@@ -7,14 +19,14 @@
 ## requested, and returns the pre-computed cached value on all subsequent 
 ## requests.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(X = matrix()) {
     inv <- NULL
     set <- function(Y) {
       X <<- Y
       inv <<- NULL
     }
     get <- function() X
-    setinv <- function(I) inv <<- inv
+    setinv <- function(I) inv <<- I
     getinv <- function() inv
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
@@ -24,7 +36,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve results in computing the inverse, and all subsequent calls return
 ## a cached value of the inverse.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(X, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- X$getinv()
     if (!is.null(inv)) {
