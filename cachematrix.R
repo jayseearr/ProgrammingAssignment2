b0bf3da3ce9aab452432ad4df8b59ec4a9b9d957
jayseearr <86374229+jayseearr@users.noreply.github.com>
2021-12-09 11:10:43 -0500

    Edits to the comments in cachematrix.R
    Clarified in the top-level comment that a cachematrix is really a list of functions; added a few comments about return value types.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
 ## cachematrix.R contains functions used in the creation of cacheMatrix 
-## objects. A cacheMatrix has the functionality of a matrix object, but stores
-## the inverse of the matrix (obtained via the 'solve' function) in a
-## persistent variable once it has been solved for once. 
+## objects. A cacheMatrix is really a list of functions that do the following:
+##   1. Set the value of the matrix
+##   2. Get the value of the matrix
+##   3. Set the inverse of the matrix 
+##   4. Get the inverse of the matrix (from cached value, if possible)
+##
+## After the inverse has been computed once (by calling the cacheSolve function), 
+## the inverse is stored in a persistent variable so that it doesn't have to be
+## computed each time cacheSolve is called. Note that the inverse should only
+## be accessed from the cacheSolve(Y) function, not from Y$getinv(); this avoids 
+## a return value of NULL if getinv is called before cacheSolve.
 ##
 ## Example usage:
 ## N <- 100
@@ -9,15 +17,14 @@
 ## Y <- makeCacheMatrix(X)       # create a cacheMatrix identical to the matrix X.  
 ## Yi <- cacheSolve(Y)           # returns the inverse of Y (analogous to solve(X)).
 ##
-## Y$get()      # returns the NxN matrix
-## Y$getinv()   # returns NULL if the inverse has not been solved for yet, and the inverse of X otherwise (equal to solve(X))
+## XX <- Y$get()                 # returns the NxN matrix (equal to X)
 
 
-## makeCacheMatrix returns a matrix that has a cached inverse. This is useful
-## for large matrices, where computing the inverse (using the 'solve' function)
-## is time consuming. A cached matrix comptues the inverse the first time it is
-## requested, and returns the pre-computed cached value on all subsequent 
-## requests.
+## makeCacheMatrix returns a "matrix" (a list of 4 functions) that can contain 
+## a cached inverse. This is useful for storing large matrices, where computing 
+## the inverse (using the 'solve' function) is time consuming. A cached matrix 
+## comptues the inverse the first time it is requested, and returns the pre-computed 
+## cached value on all subsequent requests.
 
 makeCacheMatrix <- function(X = matrix()) {
     inv <- NULL
@@ -37,7 +44,8 @@ makeCacheMatrix <- function(X = matrix()) {
 ## a cached value of the inverse.
 
 cacheSolve <- function(X, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'X'. Note that the returned value
+    ## is of class "matrix", not a cachematrix.
     inv <- X$getinv()
     if (!is.null(inv)) {
       message("getting cached data")
